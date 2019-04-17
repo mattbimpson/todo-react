26874ea81b95780e73b58fd58b4657315e1cefad
Matt <m.bimpson@gmail.com>
2019-04-17 10:10:56 +0100

    remove comments

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import initialState from './initialState';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import reducer from './reducer'; // the value from combineReducers
+import reducer from './reducer';
 
 const persistConfig = {
  key: 'root',
  storage: storage,
- stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+ stateReconciler: autoMergeLevel2
 };
 
 const pReducer = persistReducer(persistConfig, reducer);
